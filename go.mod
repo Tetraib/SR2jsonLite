9412ee826c3067200083eb7b9429048058a03dfe
Thibaut Constant <thibaut.constant@isisphinx.com>
2019-07-04 15:54:43 +0200

    completed poc

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Tetraib/SR2jsonLite
 
 go 1.12
 
-require github.com/tidwall/gjson v1.3.0 // indirect
+require (
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/tidwall/gjson v1.3.0
+)
--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+
+	"github.com/google/uuid"
+	"github.com/tidwall/gjson"
 )
 
 func main() {
+	fmt.Println("Listening dicom on port 11112...")
 	storescp := exec.Command("./third_party/dcmtk/storescp", "-od", "./dicom", "-q", "-xcr", "printArg #f", "11112")
 	// If main crash storescp still run and prevent main to restart
 	storescpOut, err := storescp.StdoutPipe()
@@ -23,17 +28,30 @@ func main() {
 	scanner.Split(bufio.ScanWords)
 	for scanner.Scan() {
 		m := scanner.Text()
-		fmt.Println(m)
 		s := fmt.Sprintf("./dicom/%s", m)
-		dcm2jsonOut, err := exec.Command("./third_party/dcmtk/dcm2json", s).Output()
+		dcm2jsonOut, err := exec.Command("./third_party/dcmtk/dcm2json", "-q", "-fc", s).Output()
 		if err != nil {
-			log.Print(err)
-		}
-		if err := os.Remove(s); err != nil {
 			log.Print(err)
 		}
 		fmt.Println(string(dcm2jsonOut))
 
+		if err := os.Remove(s); err != nil {
+			log.Print(err)
+		}
+		dlp := gjson.GetBytes(dcm2jsonOut, "0040A730.Value.15.0040A730.Value.5.0040A730.Value.2.0040A300.Value.0.0040A30A.Value.0")
+		ctdi := gjson.GetBytes(dcm2jsonOut, "0040A730.Value.15.0040A730.Value.5.0040A730.Value.0.0040A300.Value.0.0040A30A.Value")
+		accessionNumber := gjson.GetBytes(dcm2jsonOut, "00080050.Value.0")
+
+		id, err := uuid.NewUUID()
+		if err != nil {
+			log.Print(err)
+		}
+		message := []byte(fmt.Sprintf("{accessionNumber:%s,dlp:%s,ctdi:%s}", accessionNumber, dlp, ctdi))
+		path := fmt.Sprintf("./json/%s.json", id)
+
+		if err := ioutil.WriteFile(path, message, 0644); err != nil {
+			log.Print(err)
+		}
 	}
 	storescp.Wait()
 }
