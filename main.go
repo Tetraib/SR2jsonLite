9a9f3884c9c965dec0ea4e169f1b6089980aa5dd
Thibaut Constant <thibaut.constant@isisphinx.com>
2019-07-04 11:52:38 +0200

    add error management

--- main.go
+++ main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os/exec"
 )
 
 func main() {
-	cmd := exec.Command("./third_party/dcmtk/storescp", "-od", "./dicom", "-q", "-xcr", "printArg #f", "11112")
+	storescp := exec.Command("./third_party/dcmtk/storescp", "-od", "./dicom", "-q", "-xcr", "printArg #f", "11112")
+	// dcm2jsonOut, err := exec.Command("date").Output()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	stdout, _ := cmd.StdoutPipe()
-	cmd.Start()
+	storescpOut, err := storescp.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := storescp.Start(); err != nil {
+		log.Fatal(err)
+	}
 
-	scanner := bufio.NewScanner(stdout)
+	scanner := bufio.NewScanner(storescpOut)
 	scanner.Split(bufio.ScanWords)
 	for scanner.Scan() {
 		m := scanner.Text()
 		fmt.Println(m)
+
 	}
-	cmd.Wait()
+	storescp.Wait()
 }
